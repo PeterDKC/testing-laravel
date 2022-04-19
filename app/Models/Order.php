3cd27ad3cba51598d88a4a803125f5a658e058d6
Peter DeMarco <yinrunning@gmail.com>
2022-04-18 22:39:49 -0500

    Episode Start

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,19 +5,55 @@ namespace App\Models;
 use App\Models\Florist;
 use App\Models\Product;
 use App\Models\Customer;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Order extends Model
 {
     use HasFactory;
 
+    protected $taxRate = .05;
+
     protected $fillable = [
         'total',
         'note',
+        'order_number',
         'florist_id',
         'customer_id',
     ];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::creating(function ($order) {
+            $order->order_number = Str::kebab($order->customer->name . '-' . now('CST')->toDateString());
+        });
+    }
+
+    public function note(): Attribute
+    {
+        return new Attribute(
+            set: function ($note) {
+                if (strlen($note)) {
+                    return '<p><strong>Note:</strong> ' . $note . '</p>';
+                }
+
+                return '';
+            }
+        );
+    }
+
+    public function total(): Attribute
+    {
+        return new Attribute(
+            get: function ($total) {
+                return $total * $this->taxRate;
+            }
+        );
+    }
 
     public function customer()
     {
